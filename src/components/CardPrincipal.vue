b677042480a773e017d9b14f31929ac9ffa40b39
edgarmauricio123 <22620066@tlaxiaco.tecnm.mx>
2025-03-24 15:28:21 -0600

    nuevo commit

--- src/components/CardPrincipal.vue
+++ src/components/CardPrincipal.vue
@@ -3,36 +3,41 @@
 </script>
 
 <template>
- <div class="row">
-<div class="col-md-6">
-   <div class="card mt-4">
-       <div style="background-color: #f6a68d;" class="card-header">
-           <h6 class="text-white text-center">¿Qué tipos de programación web existen?</h6>
-       </div>
-       <div style="background-color: #fce4be;"  class="card-body">
-    <p class="text">Creación de sitios estáticos o dinámicos, Programación de aplicaciones para dispositivos móviles, Creación de tiendas en línea.</p>
-    <br>
-    <img class="img" src="https://st3.depositphotos.com/1007566/13259/v/450/depositphotos_132592684-stock-illustration-programmer-languages-working-laptop.jpg" alt="Imagen de programación"><br>
-    <button class="btn btn-warning">Más información</button>
-  </div>
-   </div>
-</div>
-<div class="col-md-6">
-   <div class="card mt-4">
-       <div style="background-color: #f6a68d;" class="card-header">
-           <h6 class="text-white text-center">¿Qué herramientas se usan para la programación web?</h6>
-       </div>
-       <div style="background-color: #fce4be;" class="card-body">
-<!-- agregar imagen y un texto -->
- <p>Google Trends, Hotjar, Semrush, Figma, Adobe XD, Sketch, Wix, WordPress.</p>
-  <br>
-  <img class="img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2QOjUDq0KNTjfSy4qXKK4nst7EHrhEDmq0A&s" alt="">
-  <br>
-  <button class="btn btn-warning">Más información</button>
-  <br>
-</div>
-   </div>
-</div>
-   </div>
+ 
+   <div class="row">
+       <div class="cold-md-12">
+           <div class="card mt-4">
+               <div style="background-color: #bf9f88;" class="card-header">
+                   <h6 class="text-white text-center" >Programacion web</h6>
+               </div>
+               
+               <div style="background-color: #e8c8a1;" class="card-body">
+                   <p class="text">
+                    La programación web es el proceso de crear sitios web y aplicaciones web usando lenguajes de programación. 
+                    El objetivo es que los sitios sean atractivos, funcionales, interactivos y dinámicos.
+                   </p>
+                   <h1 class="text1">¿Qué se hace en la programación web? </h1>
+                   <p class="text">Crear códigos fuente, Estructurar páginas, Crear archivos multimedia, Desarrollar funcionalidades que 
+                    permitan a los usuarios interactuar con la web.</p>
+                    <h1 class="text1">¿Qué lenguajes se usan en la programación web?</h1>
+                    <p class="text">-HTML (HyperText Markup Language)</p>
+                    <p class="text">-CSS (Cascading Style Sheets)</p>
+                    <p class="text">-JavaScript</p>
+                    <p class="text">-C, C++, Go, Java, Python, PHP</p>
 
+               </div>
+
+           </div>
+       </div>
+   </div>
 </template>
+<style>
+.text{
+  text-align: center;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+.text1{
+  text-align: center;
+  font-family: 'Times New Roman', Times, serif;  text-align: center;
+  color: blueviolet;
+}</style>
